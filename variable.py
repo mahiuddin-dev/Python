75c2b0b0e6fd24fee23823a443db4baefd92cb98
codermahiuddin <codermahiuddin@gmail.com>
2022-03-03 22:58:10 +0600

    variable.py added

--- variable.py
+++ variable.py
@@ -1,6 +1,41 @@
+"""
+Variables are nothing but reserved memory locations to store values. It means that when you create a variable, you reserve some space in the memory. Based on the data type of a variable, the interpreter allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to the variables, you can store integers, decimals or characters in these variables.Python has no command for declaring a variable. 
 
-x = int(input("Please enter an integer: "))
+"""
 
-ans3 = x / 2
+#? Variable Names
 
-print(ans3)
+"""
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ) 
+
+"""
+"""
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+Multi Words Variable Names
+Variable names with more than one word can be difficult to read. There are several techniques you can use to make them more readable:
+"""
+
+# Camel Case
+"""
+Each word, except the first, starts with a capital letter. For Example
+myVariableName = "Python" 
+"""
+
+# Pascal Case
+"""
+Each word starts with a capital letter.  For Example
+MyVariableName = "Python"
+"""
+
+"""
+Pascal case and camel case are two of the most often referenced. Both demand variables made from compound words and have the first letter of each appended word written with an uppercase letter. The difference is that Pascal case requires the first letter to be uppercase as well, while camelcase does not.
+"""
+
+# Snake Case
+"""
+Each word is separated by an underscore character.  For Example
+my_variable_name = "Python"
+"""
